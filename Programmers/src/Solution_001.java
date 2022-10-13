7f3956ac91c2717fc3e97a88f2649631442b13e0
KathleenJung <ovo0325@outlook.kr>
2022-10-13 17:48:25 +0900

    Programmers [코딩테스트 입문] 001. 두수의 곱

---
+++ Programmers/src/Solution_001.java
@@ -0,0 +1,12 @@
+public class Solution_001 {
+
+	public static void main(String[] args) {
+		System.out.println(solution(3, 4));
+		System.out.println(solution(27, 19));
+		
+	}
+	public static int solution(int num1, int num2) {
+        int answer = num1 * num2;
+        return answer;
+    }
+}
