27e77f0c27479a0af3c3dc2674f21a7bb87475eb
KathleenJung <ovo0325@outlook.kr>
2022-10-13 17:49:53 +0900

    Programmers [코딩테스트 입문] 002. 나머지 구하기

---
+++ Programmers/src/Solution_002.java
@@ -0,0 +1,12 @@
+
+public class Solution_002 {
+
+	public static void main(String[] args) {
+		System.out.println(solution(3, 2));
+		System.out.println(solution(10, 5));
+	}
+	public static int solution(int num1, int num2) {
+        int answer = num1%num2;
+        return answer;
+    }
+}
