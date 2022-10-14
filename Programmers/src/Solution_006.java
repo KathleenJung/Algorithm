2b4f3200a7b14d7be3f985b7e7929e24a638b08d
KathleenJung <ovo0325@outlook.kr>
2022-10-14 22:28:02 +0900

    Programmers [코딩테스트 입문] 006. 나이 출력

---
+++ Programmers/src/Solution_006.java
@@ -0,0 +1,14 @@
+
+public class Solution_006 {
+
+	public static void main(String[] args) {
+		System.out.println(solution(40));
+		System.out.println(solution(23));
+		
+	}
+	public static int solution(int age) {
+        int answer = 2023-age;
+        return answer;
+    }
+
+}
--- SWEA/src/Solution_14510.java
+++ SWEA/src/Solution_14510.java
@@ -43,10 +43,10 @@ public class Solution_14510 {
 					}
 				}
 			}
-			for (int i = 0; i < arr.length; i++) {
-				System.out.print(arr[i] + " ");
-			}
-			System.out.println();
+//			for (int i = 0; i < arr.length; i++) {
+//				System.out.print(arr[i] + " ");
+//			}
+//			System.out.println();
 		}
 
 	}
