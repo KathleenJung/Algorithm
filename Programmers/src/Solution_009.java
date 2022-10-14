6be609db5b8e460386ebe0855eeefd1b95a12bd4
KathleenJung <ovo0325@outlook.kr>
2022-10-14 22:39:14 +0900

    Programmers [코딩테스트 입문] 009. 배열의 평균값

--- Programmers/src/Solution_009.java
+++ Programmers/src/Solution_009.java
@@ -2,8 +2,17 @@
 public class Solution_009 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] numbers = new int[] {1,2,3,4,5,6,7,8,9,10};
+		System.out.println(solution(numbers));
+		numbers=new int[] {89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99};
+		System.out.println(solution(numbers));
 	}
-
+	public static double solution(int[] numbers) {
+        double answer = 0;
+        for (int i = 0; i < numbers.length; i++) {
+			answer += numbers[i];
+		}
+        answer /= numbers.length;
+        return answer;
+    }
 }
