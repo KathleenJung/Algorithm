6ab88b465560091f7f9551e0d3121e2df3a72102
KathleenJung <ovo0325@outlook.kr>
2022-10-14 22:29:56 +0900

    Programmers [코딩테스트 입문] 007. 숫자 비교하기

--- Programmers/src/Solution_006.java
+++ Programmers/src/Solution_006.java
@@ -4,11 +4,12 @@ public class Solution_006 {
 	public static void main(String[] args) {
 		System.out.println(solution(40));
 		System.out.println(solution(23));
-		
+
 	}
+
 	public static int solution(int age) {
-        int answer = 2023-age;
-        return answer;
-    }
+		int answer = 2022 - age + 1;
+		return answer;
+	}
 
 }
