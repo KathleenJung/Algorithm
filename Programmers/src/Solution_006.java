4414a98c47fe7f330ea000af22f629c5c033c8ca
KathleenJung <85939045+KathleenJung@users.noreply.github.com>
2022-10-17 23:24:34 +0900

    Programmers [코딩테스트 입문] 006. 나이 출력

--- Programmers/src/Solution_006.java
+++ Programmers/src/Solution_006.java
@@ -4,7 +4,6 @@ public class Solution_006 {
 	public static void main(String[] args) {
 		System.out.println(solution(40));
 		System.out.println(solution(23));
-
 	}
 
 	public static int solution(int age) {
