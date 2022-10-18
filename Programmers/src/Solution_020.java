22ac2b6552daf76cd133f2f815a8181b17d5ea85
KathleenJung <ovo0325@outlook.kr>
2022-10-19 01:19:22 +0900

    Programmers [코딩테스트 입문] 021. 아이스 아메리카노

--- Programmers/src/Solution_020.java
+++ Programmers/src/Solution_020.java
@@ -1,4 +1,4 @@
-
+// Programmers [코딩테스트 입문] 020. 옷가게 할인 받기
 public class Solution_020 {
 
 	public static void main(String[] args) {
@@ -8,12 +8,14 @@ public class Solution_020 {
 
 	public static int solution(int price) {
 		int answer = 0;
-		if(price >= 500000) {
+		if (price >= 500000) {
 			answer = price *= 0.8;
-		} else if(price >= 300000) {
+		} else if (price >= 300000) {
 			answer = price *= 0.9;
-		} else if(price >= 100000) {
+		} else if (price >= 100000) {
 			answer = price *= 0.95;
+		} else {
+			answer = price;
 		}
 		return answer;
 	}
---
+++ Programmers/src/Solution_021.java
@@ -0,0 +1,17 @@
+// Programmers [코딩테스트 입문] 021. 아이스 아메리카노
+
+public class Solution_021 {
+
+	public static void main(String[] args) {
+		System.out.println(solution(5500));
+		System.out.println(solution(15000));
+	}
+	
+	public static int[] solution(int money) {
+        int[] answer = {0,0};
+        answer[0] = money/5500;
+        answer[1] = money%5500;
+        return answer;
+    }
+
+}
