78d5ef6bc201ad24f371cb91c8555953220ddf7d
KathleenJung <ovo0325@outlook.kr>
2022-10-22 01:01:38 +0900

    Programmers [코딩테스트 입문] 029. 짝수의 합

--- Programmers/src/Solution_029.java
+++ Programmers/src/Solution_029.java
@@ -8,13 +8,13 @@ public class Solution_029 {
 	}
 
 	public static int solution(int n) {
-        int answer = 0;
-        for (int i = 1; i <= n; i++) {
-			if(i%2==0) {
+		int answer = 0;
+		for (int i = 1; i <= n; i++) {
+			if (i % 2 == 0) {
 				answer += i;
 			}
 		}
-        return answer;
-    }
+		return answer;
+	}
 
 }
