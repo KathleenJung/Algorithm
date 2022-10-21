605ce66d1da1a44c894da1dbc3054ae137480bbe
KathleenJung <ovo0325@outlook.kr>
2022-10-21 23:40:15 +0900

    Programmers [코딩테스트 입문] 029. 짝수의 합

---
+++ Programmers/src/Solution_029.java
@@ -0,0 +1,20 @@
+// Programmers [코딩테스트 입문] 029. 짝수의 합
+
+public class Solution_029 {
+
+	public static void main(String[] args) {
+		System.out.println(solution(10));
+		System.out.println(solution(4));
+	}
+
+	public static int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+			if(i%2==0) {
+				answer += i;
+			}
+		}
+        return answer;
+    }
+
+}
