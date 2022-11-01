7e40ceed1211e9017790008e705f7ccfa18c4b60
KathleenJung <ovo0325@outlook.kr>
2022-11-01 22:03:50 +0900

    BOJ#11021. A+B - 7

--- BOJ/src/Main_11021.java
+++ BOJ/src/Main_11021.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main_11021 {
-//	B5 A+B -7
+//	B5 A+B - 7
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
