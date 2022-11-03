827ab1fee17cba42437e07904594b227444ab977
KathleenJung <ovo0325@outlook.kr>
2022-11-04 00:59:24 +0900

    BOJ#10952. A+B - 5

--- BOJ/src/Main_10952.java
+++ BOJ/src/Main_10952.java
@@ -6,13 +6,13 @@ public class Main_10952 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int A, B;
-		while(true) {
+		while (true) {
 			A = sc.nextInt();
 			B = sc.nextInt();
-			if(A==0 && B==0) {
+			if (A == 0 && B == 0) {
 				break;
 			}
-			System.out.println(A+B);
+			System.out.println(A + B);
 		}
 		sc.close();
 	}
