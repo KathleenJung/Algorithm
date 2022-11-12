90c4ff9d8d8d6476d20b633a62cddd80b32495fa
KathleenJung <ovo0325@outlook.kr>
2022-11-12 21:26:41 +0900

    BOJ#1931. 회의실 배정

--- BOJ/src/Main_1931.java
+++ BOJ/src/Main_1931.java
@@ -20,7 +20,7 @@ public class Main_1931 {
 
 			@Override
 			public int compare(int[] o1, int[] o2) {
-				if(o1[1] == o2[1]) {
+				if (o1[1] == o2[1]) {
 					return o1[0] - o2[0];
 				}
 				return o1[1] - o2[1];
@@ -29,7 +29,7 @@ public class Main_1931 {
 		int cnt = 0;
 		int f = 0;
 		for (int i = 0; i < N; i++) {
-			if(f <= arr[i][0]) {
+			if (f <= arr[i][0]) {
 //				System.out.println(arr[i][0] + " " + arr[i][1]);
 				f = arr[i][1];
 				cnt++;
