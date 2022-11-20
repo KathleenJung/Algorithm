5867ac9fc5fcac24931d450fff21223f5cb606f1
KathleenJung <ovo0325@outlook.kr>
2022-11-21 03:16:32 +0900

    SWEA#4193. 수영대회 결승전 ( 완전 탐색 + 구현 )

--- SWEA/src/Solution_4193.java
+++ SWEA/src/Solution_4193.java
@@ -1,12 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution_4193 {
+	static class Point {
+		int x, y;
+
+		public Point(int x, int y) {
+			super();
+			this.x = x;
+			this.y = y;
+		}
+	}
 	static int N;
 	static int[][] map;
 	static int A, B, C, D;
+	static int[][] delta = {{-1,0},{0,1},{1,0},{0,-1}};
+	static boolean[][] visited;
+	static int ans;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
@@ -25,6 +39,40 @@ public class Solution_4193 {
 			st = new StringTokenizer(br.readLine());
 			C = Integer.parseInt(st.nextToken());
 			D = Integer.parseInt(st.nextToken());
+			
+			ans = -1;
+			
+			solve();
+			
+			System.out.println(ans);
+		}
+	}
+	private static void solve() {
+		Queue<Point> q = new ArrayDeque<>();
+		visited = new boolean[N][N];
+		visited[A][B] = true;
+		int time = 0;
+		q.offer(new Point(A, B));
+		
+		while(!q.isEmpty()) {
+			time++;
+			int size = q.size();
+			for (int s = 0; s < size; s++) {
+				Point p = q.poll();
+				for (int d = 0; d < 4; d++) {
+					int nx = p.x+delta[d][0];
+					int ny = p.y + delta[d][1];
+					if(nx>=0&&ny>=0&&nx<N&&ny<N&&(map[nx][ny]==0||(map[nx][ny]==2 && time %3 == 2))&&!visited[nx][ny]) {
+						visited[nx][ny] = true;
+						q.offer(new Point(nx, ny));
+						System.out.println(nx + " " + ny);
+						if(nx == C && ny == D) {
+							ans = time;
+							return;
+						}
+					}
+				}
+			}
 		}
 	}
 
