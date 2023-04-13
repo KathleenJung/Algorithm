b03f7f482c977c5862888d9448d5c5c11a759985
KathleenJung <ovo0325@outlook.kr>
2023-04-13 17:01:54 +0900

    BOJ#10809. 알파벳 찾기

--- BOJ/src/Main_10809.java
+++ BOJ/src/Main_10809.java
@@ -1,17 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main_10809 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String str = sc.next();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
 		int[] arr = new int[26];
-//		for(int i=0;i<str.length();i++) {
-//			if(str.charAt(i) == ) {
-//				arr[i]
-//			}
-//		}
-		
+		for (int i = 0; i < str.length(); i++) {
+//			System.out.println(str.charAt(i)-'0'-49);
+			if (arr[str.charAt(i) - '0' - 49] == 0) {
+				arr[str.charAt(i) - '0' - 49] = i + 1;
+			}
+		}
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] == 0 ? "-1 " : arr[i] - 1 + " ");
+		}
 	}
 
 }
