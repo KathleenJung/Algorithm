e101e769d6b17ea8c36b455d2823923c6ffedccb
KathleenJung <ovo0325@outlook.kr>
2023-07-29 22:00:14 +0900

    BOJ#10944. 랜덤 게임~~

---
+++ BOJ/src/Main_10944.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Main_10944 {
+
+	public static void main(String[] args) {
+        Random random = new Random();
+        int n = random.nextInt(10000) + 1;
+        System.out.println(n);
+	}
+
+}
